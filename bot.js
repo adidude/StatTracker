47e1234d2165527b52334da3af8098d0979e9ca2
adidude <menon.or.adithya@gmail.com>
2019-08-03 19:53:47 +0530

    Added Modules
    The API's utilized were not in the repo.

---
+++ bot.js
@@ -0,0 +1,63 @@
+/** Import discord.js API library and login token.
+*/
+const Discord = require('discord.js');
+const fs = require('fs');
+const { token } = require('./auth.json');
+const client = new Discord.Client();
+
+// Will collect data
+function collectData(connection) {
+	// Write data to file
+	let isWritten = false;
+	// Loop as long as data is not written
+	while (!isWritten) {
+		try {
+			// Adds data to file.
+			fs.appendFile('Connections.dat', connection.date + ',' + connection.time + ',' + connection.id + ',' + connection.isConnected + ',' + connection.tag + '\n', (e) =>{
+				if (e) {throw e;}
+			});
+			isWritten = true;
+		}
+		catch (e) {
+			console.log('Write failed. Retrying writing to  file.');
+		}
+	}
+}
+
+// Inform us when connected to server.
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// TODO: message based stuff, music.
+/* client.on('message', message => {
+	message.reply('Yes I hear you.');
+});*/
+
+
+// When a user leaves/joins a voice channel
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+	// Collect date/time information.
+	const date = new Date();
+	const month = date.getMonth() + 1;
+	const today = date.getDate() + '-' + month + '-' + date.getFullYear();
+	const now = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
+	// If a user joins
+	if (typeof oldMember.voiceChannel === 'undefined' && newMember.selfMute === false && newMember.selfDeaf === false && newMember.serverMute === false && newMember.serverDeaf === false) {
+		// Create voiceConnection object which stores connection details.
+		const voiceConnection = { date:today, time:now, tag:newMember.user.tag, id:newMember.id, isConnected:true };
+		// Collect the data.
+		collectData(voiceConnection);
+	}
+	else if (typeof newMember.voiceChannel === 'undefined') {
+		// If user leaves.
+		// Create voiceConnection object which stores connection details.
+		const voiceConnection = { date:today, time:now, tag:newMember.user.tag, id:newMember.id, isConnected:false };
+		// Collect the data.
+		collectData(voiceConnection);
+	}
+});
+
+
+client.login(token);
