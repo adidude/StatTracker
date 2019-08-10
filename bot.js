090a7f2b43e938488c977a6f833652130e5c1793
adidude <menon.or.adithya@gmail.com>
2019-08-10 19:45:19 +0530

    Now Collects timestamps properly
    Before date/time was collected seperatly.

--- bot.js
+++ bot.js
@@ -17,9 +17,9 @@ database.connect();
 function collectData(connection) {
 	// TODO: Specify in SQL that DMY mode is used for DateStyle parameter.
 	// Adds data to file. This is PostgreSQL. New entries are added to the table voiceStateChanges.
-	database.query('INSERT INTO voiceStateChanges(date, time, tag, id, isConnected, isMuted, isDeaf) VALUES ($1,$2,$3,$4,$5,$6,$7)', connection);
+	database.query('INSERT INTO voiceStateConnections(timestamp, tag, id, isConnected, isMuted, isDeaf) VALUES ($1,$2,$3,$4,$5,$6)', connection);
 	// The Columns in the table are in the order:
-	// | Date | Time | Tag | ID | isConnected | isMuted | isDeaf |
+	// | Timestamp | Tag | ID | isConnected | isMuted | isDeaf |
 }
 
 // Inform us when connected to server.
@@ -35,26 +35,25 @@ client.once('ready', () => {
 
 // When a user leaves/joins a voice channel
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-	// Collect date/time information.
+	// Collect date/time information in UTC.
 	const date = new Date();
-	const month = date.getMonth() + 1;
-	const today = date.getDate() + '-' + month + '-' + date.getFullYear();
-	const now = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+	const month = date.getUTCMonth() + 1;
+	const timestamp = date.getUTCDate() + '-' + month + '-' + date.getUTCFullYear() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes() + ':' + date.getUTCSeconds() + ' UTC';
 
 	// If a user joins or changes mute/deafened state.
 	if ((typeof oldMember.voiceChannel === 'undefined' && newMember.voiceChannel != null) || oldMember.mute != newMember.mute || oldMember.deaf != newMember.deaf) {
 		// Create voiceConnection object which stores connection details.
 		// The Columns in the table are in the order:
-		// | Date | Time | Tag | ID | isConnected | isMuted | isDeaf |
-		const voiceConnection = [ today, now, newMember.user.tag, newMember.id, true, newMember.mute, newMember.deaf ];
+		// | Timestamp | Tag | ID | isConnected | isMuted | isDeaf |
+		const voiceConnection = [ timestamp, newMember.user.tag, newMember.id, true, newMember.mute, newMember.deaf ];
 		// Collect the data.
 		collectData(voiceConnection);
 	} // If user leaves.
 	else if (typeof newMember.voiceChannel === 'undefined') {
 		// Create voiceConnection object which stores connection details.
 		// The Columns in the table are in the order:
-		// | Date | Time | Tag | ID | isConnected | isMuted | isDeaf |
-		const voiceConnection = [ today, now, newMember.user.tag, newMember.id, false, newMember.mute, newMember.deaf ];
+		// | Timestamp | Tag | ID | isConnected | isMuted | isDeaf |
+		const voiceConnection = [ timestamp, newMember.user.tag, newMember.id, false, newMember.mute, newMember.deaf ];
 		// Collect the data.
 		collectData(voiceConnection);
 	}
