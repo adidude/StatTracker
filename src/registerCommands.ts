65a7be75d90bd6d4042eddcdcca762daf2b2d9e0
Aditya Kumar Menon <menon.or.adithya@gmail.com>
2021-09-24 04:12:33 +0300

    RegisterCommands made for registering slash commands

---
+++ src/registerCommands.ts
@@ -0,0 +1,39 @@
+// Only needs to be run once to register new commands.
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Client, Intents } from 'discord.js';
+import { guildID, token } from '../config/config.json';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+
+// All commands can be added here
+const actions = [
+	new SlashCommandBuilder().setName('test').setDescription('Tests absolutly nothing'),
+	new SlashCommandBuilder().setName('test1').setDescription('Tests another command'),
+].map(command => command.toJSON());
+
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] });
+
+client.on('ready', () =>
+{
+	registerCommands();
+});
+
+async function registerCommands()
+{
+	const rest = new REST({ version: '9' }).setToken(token);
+
+	try
+	{
+		await rest.put(
+			Routes.applicationGuildCommands(client.application!.id, guildID),
+			{ body: actions },
+		);
+		client.destroy();
+	}
+	catch (err)
+	{
+		console.log(err);
+	}
+}
+
+client.login(token);
